4544db1b2460d6de6e6bfed831dabb6456b82091
Cayu <cayu@cayu.com.ar>
2022-04-23 01:38:35 -0300

    Create stack2.c

---
+++ PEN-200/Lab/BoF/Linux/h4ckbl0g/stack2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int cookie;
+    char buf[80];
+
+    printf("buf: %08x cookie: %08x\n", &buf, &cookie);
+    gets(buf);
+
+    if (cookie == 0x01020305)
+        printf("you win!\n");
+}
